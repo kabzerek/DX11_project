d50df454ff7f96e054cbecabfc6eb8c046fc05ab
Bajter <bajter@o2.pl>
2013-10-28 09:00:41 +0100

    Dodałem oświetlenie ambient i specular. Wszystko powinno działać fajnie, będzie lepiej widać, gdy załadujemy jakiś model. Zmiany dotyczą przede wszystkim: GraphicsClass LightClass LightShaderClass Light.ps Light.vs
    Signed-off-by: Bajter <bajter@o2.pl>

---
+++ PwAG/PwAG/GraphicsClass.h
@@ -0,0 +1,39 @@
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+
+#include "D3DClass.h"
+#include "CameraClass.h"
+#include "ModelClass.h"
+#include "LightShaderClass.h"
+#include "LightClass.h"
+
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render(float rotation);
+
+	D3DClass* m_D3D;
+	CameraClass* m_Camera;
+	ModelClass* m_Model;
+	LightShaderClass* m_LightShader;
+	LightClass* m_Light;
+};
+
+#endif
